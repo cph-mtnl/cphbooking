7fc085a94ff5c9d8ba5f625b1159c5a5e7daff31
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2024-11-28 17:31:19 +0100

    Updates

--- assets/index-ba6SUGEc.js
+++ assets/index-ba6SUGEc.js
@@ -1,4 +1,4 @@
-import{w as pn,R as Oe}from"./index-BzsLWBy9.js";/*!
+import{w as pn,R as Oe}from"./index-eUQrCII0.js";/*!
  * Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
